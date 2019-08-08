9324a96356c3c259814ae66280f1ac9fb4401d68
lawtech0902 <584563542@qq.com>
2019-08-08 11:02:06 +0800

    initialize project structure

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import store from './store'
+
+import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
+// 解决移动端click事件300ms延迟问题
+fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
+
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
